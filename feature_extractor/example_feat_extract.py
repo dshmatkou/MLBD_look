040386072a82b83dd4b84e5d167d7a542e8057d7
anastasiya-zhyrkevich <anastasiya.zhyrkevich@gmail.com>
2019-04-29 01:06:34 +0300

    model API

--- feature_extractor/example_feat_extract.py
+++ feature_extractor/example_feat_extract.py
@@ -111,6 +111,81 @@ def feature_extraction_queue(feature_extractor, image_path, layer_names,
     return feature_dataset
 
 
+def raw_feature_extraction_queue(feature_extractor, raw_image, layer_names,
+                             batch_size, num_classes, num_images=100000):
+    '''
+    Given a directory containing images, this function extracts features
+    for all images. The layers to extract features from are specified
+    as a list of strings. First, we seek for all images in the directory,
+    sort the list and feed them to the filename queue. Then, batches are
+    processed and features are stored in a large object `features`.
+
+    :param feature_extractor: object, TF feature extractor
+    :param raw_image: str, path to directory containing images
+    :param layer_names: list of str, list of layer names
+    :param batch_size: int, batch size
+    :param num_classes: int, number of classes for ImageNet (1000 or 1001)
+    :param num_images: int, number of images to process (default=100000)
+    :return:
+    '''
+
+    '''
+    # Add a list of images to process, note that the list is ordered.
+    image_files = [raw_image]
+
+    num_examples = len(image_files)
+    num_batches = int(np.ceil(num_examples/batch_size))
+
+    # Fill-up last batch so it is full (otherwise queue hangs)
+    utils.fill_last_batch(image_files, batch_size)
+
+    print("#"*80)
+    print("Batch Size: {}".format(batch_size))
+    print("Number of Examples: {}".format(num_examples))
+    print("Number of Batches: {}".format(num_batches))
+    '''
+
+
+    # Add all the images to the filename queue
+    num_examples = 1
+    num_batches = 1
+    feature_extractor.set_image_raw(raw_image)
+
+    # Initialize containers for storing processed filenames and features
+    feature_dataset = {'filenames': []}
+    for i, layer_name in enumerate(layer_names):
+        layer_shape = feature_extractor.layer_size(layer_name)
+        layer_shape[0] = 1  # replace ? by number of examples
+        feature_dataset[layer_name] = np.zeros(layer_shape, np.float32)
+        print("Extracting features for layer '{}' with shape {}".format(layer_name, layer_shape))
+
+    print("#"*80)
+
+    # Perform feed-forward through the batches
+    for batch_index in range(num_batches):
+
+        t1 = time.time()
+
+        # Feed-forward one batch through the network
+        outputs = feature_extractor.feed_forward_batch(layer_names)
+
+        feature_dataset[layer_name] =  outputs[layer_name]
+
+        # Save the filenames of the images in the batch
+        # feature_dataset['filenames'].extend(outputs['filenames'])
+
+        t2 = time.time()
+        examples_in_queue = outputs['examples_in_queue']
+        examples_per_second = batch_size/float(t2-t1)
+
+        print("[{}] Batch {:04d}, Batch Size = {}, Examples in Queue = {}, Examples/Sec = {:.2f}".format(
+            datetime.now().strftime("%Y-%m-%d %H:%M"), batch_index+1,
+            batch_size, examples_in_queue, examples_per_second
+        ))
+
+    return feature_dataset
+
+
 ################################################################################
 ################################################################################
 ################################################################################
---
+++ feature_extractor/model.py
@@ -0,0 +1,50 @@
+from feature_extractor.raw_feature_extractor import FeatureExtractor
+from example_feat_extract import raw_feature_extraction_queue
+
+
+class Model(object):
+    def __init__(self, path_to_checkpoint):
+        """
+        path_to_checkpoint: path to pre-trained checkpoint file
+        """
+        self.path_to_checkpoint = path_to_checkpoint
+        self.network_name = 'resnet_v1_101'
+        self.batch_size = 1
+
+        # size of vector
+        self.num_classes = 1000
+        self.preproc_func = None
+        self.num_preproc_threads = 2
+        self.layer_names = ["resnet_v1_101/logits"]
+
+    def extract_features(self, image_as_matrix):
+
+        # Initialize the feature extractor
+        feature_extractor = FeatureExtractor(
+            network_name=self.network_name,
+            checkpoint_path=self.path_to_checkpoint,
+            batch_size=self.batch_size,
+            num_classes=self.num_classes,
+            preproc_func_name=self.preproc_func,
+            preproc_threads=self.num_preproc_threads
+        )
+
+        # Feature extraction example using a filename queue to feed images
+        feature_dataset = raw_feature_extraction_queue(
+            feature_extractor, image_as_matrix, self.layer_names,
+            self.batch_size, self.num_classes
+        )
+
+        layer_name = self.layer_names[0]
+
+        return feature_dataset[layer_name]
+
+
+if __name__ == "__main__":
+    model = Model('./checkpoints/resnet_v1_101_2016_08_28/resnet_v1_101.ckpt')
+
+    image_str = None
+    with open('./data/rider-1.jpg', 'rb') as f:
+        image_str = f.read()
+
+    model.extract_features(image_str)
