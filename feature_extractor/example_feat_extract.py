33fad312ca33f93cc18b54d644953267a1efd2c1
anastasiya-zhyrkevich <anastasiya.zhyrkevich@gmail.com>
2019-04-29 23:44:00 +0300

    import in feature extractor

--- feature_extractor/example_feat_extract.py
+++ feature_extractor/example_feat_extract.py
@@ -21,8 +21,12 @@ import numpy as np
 import time
 from datetime import datetime
 
-from feature_extractor.feature_extractor.feature_extractor import FeatureExtractor
-import feature_extractor.feature_extractor.utils as utils
+if __name__ == "__main__":
+    from feature_extractor.feature_extractor import FeatureExtractor
+    import feature_extractor.utils as utils
+else:
+    from feature_extractor.feature_extractor.feature_extractor import FeatureExtractor
+    import feature_extractor.feature_extractor.utils as utils
 
 
 def feature_extraction_queue(feature_extractor, image_path, layer_names,
